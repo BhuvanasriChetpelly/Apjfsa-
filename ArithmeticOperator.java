c5e20d91126b56d2014d26159fc482fc15f11a9e
BhuvanasriChetpelly <bhuvanasrichetpelly23@gmail.com>
2024-07-21 21:07:17 +0530

    Add files via upload

---
+++ ArithmeticOperator.java
@@ -0,0 +1,34 @@
+package basic_programs_of_java;
+
+public class ArithmeticOperator {
+
+	// Main method
+	public static void main(String[] args) {
+
+		// Initialize two integer variables with values 20 and 10.
+		int a = 20;
+		int b = 10;
+
+		// Perform and display the result of addition.
+		// Adding the values of a and b, then printing the result.
+		System.out.println("Sum of a and b is " + (a + b)); // Addition: 20 + 10 = 30
+
+		// Perform and display the result of subtraction.
+		// Subtracting the value of b from a, then printing the result.
+		System.out.println("Subtraction of a and b is " + (a - b)); // Subtraction: 20 - 10 = 10
+
+		// Perform and display the result of multiplication.
+		// Multiplying the values of a and b, then printing the result.
+		System.out.println("Multiplication of a and b is " + (a * b)); // Multiplication: 20 * 10 = 200
+
+		// Perform and display the result of division.
+		// Dividing the value of a by b, then printing the quotient.
+		System.out.println("Quotient of a and b is " + (a / b)); // Division: 20 / 10 = 2
+
+		// Perform and display the result of modulus operation.
+		// Calculating the remainder of the division of a by b, then printing the
+		// result.
+		System.out.println("Remainder of a and b is " + (a % b)); // Modulus: 20 % 10 = 0
+
+	}
+}
